f981557d3c2bc99602ab72906f264c84e1e29fb8
Ivan <ivan@plugion.org>
2021-10-11 12:49:22 +0300

    Update example.js

--- example.js
+++ example.js
@@ -2,11 +2,7 @@ jQuery(document).ready(function( $ ){
     jQuery('#wbk-service-id').val(jQuery('#wbk-service-id option:eq(1)').val());
     jQuery('#wbk-service-id').trigger('change');  
 });
-jQuery(document).on( 'wbk_picker_initialized', function(event, data) {	
-    data = data.disabilities.split(';');
-  	data = data[0].split(',');
- 	var date = new Date(data[0], data[1], data[2]);
-    var picker = jQuery('#wbk-date').pickadate('picker');
-    picker.set('select', date);
+jQuery(document).on( 'wbk_date_dropdown_initialized', function(){     
+    jQuery('.wbk_date').val( jQuery('.wbk_date option:eq(1)').val() );
+  	jQuery('.wbk_date').trigger('change');
 });
- 
